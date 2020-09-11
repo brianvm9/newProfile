6bd506a28f1ef8add9eadfc23fc1e75192f0df89
brianvm9 <brianvm93@outlook.com>
2020-09-11 16:07:32 -0700

    added more

--- js/script.js
+++ js/script.js
@@ -2,23 +2,19 @@ function selectMode(mode) {
 
 var color = "rgb(209,209,209)";
 var label = "Light Mode";
-var icon = "img/lightbulb.png";
 var name = "Light"
 if(mode === "dark"){
     color="rgb(189,133,248)";
     label="Dark Mode";
-    image = "img/moon.jpg"
     name ="Dark"
-} else{
+} else if(mode==="light"){
     color = "rgb(209,209,209)";
     label = "Light Mode";
-    image = "img/lightbulb.png"
 }
 
 console.log(label);
 
-document.getElementById("icon").src = image;
 document.getElementById("theme").style.backgroundColor = color;
 document.getElementById("label").innerHTML = label;
-document.getElementById("name").innerText = name
+document.getElementById("name").innerText = name;
 }
