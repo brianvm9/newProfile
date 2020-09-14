32a5f5553c169838ffc141854df9d4ee0d095e65
brianvm9 <brianvm93@outlook.com>
2020-09-14 15:16:33 -0700

    fixed dark mode

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@ function selectMode(mode) {
 
 var color = "rgb(209,209,209)";
 var label = "Light Mode";
-var name = "Light"
+var name = "Light";
+var text = " rgb(189,133,248)";
 if(mode === "dark"){
-    color="rgb(189,133,248)";
+    color="rgb(33,33,33)";
     label="Dark Mode";
-    name ="Dark"
+    name ="Dark";
+    text = "rgb(189,133,248)";
 } else if(mode==="light"){
     color = "rgb(209,209,209)";
     label = "Light Mode";
@@ -17,4 +19,5 @@ console.log(label);
 document.getElementById("theme").style.backgroundColor = color;
 document.getElementById("label").innerHTML = label;
 document.getElementById("name").innerText = name;
+document.getElementById("name").style.color = text;
 }
