f3afcee910f4aeacb7f6344cfbbea332f21c7bec
brianvm9 <brianvm93@outlook.com>
2020-09-16 07:36:18 -0700

    changed picture

--- js/script.js
+++ js/script.js
@@ -2,22 +2,25 @@ function selectMode(mode) {
 
 var color = "rgb(209,209,209)";
 var label = "Light Mode";
-var name = "Light";
 var text = " rgb(189,133,248)";
+var image = "img/astrodesigns.png";
+var nav = ""
 if(mode === "dark"){
     color="rgb(33,33,33)";
     label="Dark Mode";
-    name ="Dark";
     text = "rgb(189,133,248)";
-} else if(mode==="light"){
+    image = "img/astrodesignsdark.png";
+} else if(mode === "light"){
     color = "rgb(209,209,209)";
     label = "Light Mode";
+    text = "rgb(33,33,33)";
+    image = "img/astrodesigns.png";
 }
 
 console.log(label);
 
+document.getElementById("logo").src = image;
 document.getElementById("theme").style.backgroundColor = color;
 document.getElementById("label").innerHTML = label;
-document.getElementById("name").innerText = name;
-document.getElementById("name").style.color = text;
 }
+
