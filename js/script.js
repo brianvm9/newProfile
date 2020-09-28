216a3b88dd507592c5ebe4950c58f3d7ca4e1317
brianvm9 <brianvm93@outlook.com>
2020-09-28 16:38:42 -0700

    stuff changed

--- js/script.js
+++ js/script.js
@@ -4,13 +4,12 @@ var color = "rgb(209,209,209)";
 var label = "Light Mode";
 var text = " rgb(189,133,248)";
 var image = "img/astrodesigns.png";
-var nav = ""
-if(mode === "dark"){
+if(mode == "dark"){
     color="rgb(33,33,33)";
     label="Dark Mode";
     text = "rgb(189,133,248)";
     image = "img/astrodesignsdark.png";
-} else if(mode === "light"){
+} else if(mode == "light"){
     color = "rgb(209,209,209)";
     label = "Light Mode";
     text = "rgb(33,33,33)";
@@ -23,4 +22,18 @@ document.getElementById("logo").src = image;
 document.getElementById("theme").style.backgroundColor = color;
 document.getElementById("label").innerHTML = label;
 }
+
+function changeCSS(cssFile, cssLinkIndex) {
+    var oldLink = document.getElementsByTagName("link").item(cssLinkIndex);
+
+    var newLink = document.createElement("link");
+    newLink.setAttribute("rel", "stylesheet");
+    newLink.setAttribute("type", "text/css");
+    newLink.setAttribute("href", cssFile);
+
+    document.getElementsByTagName("head").item(0).replaceChild(newLink, oldLink);
+
+}
+
+
 
